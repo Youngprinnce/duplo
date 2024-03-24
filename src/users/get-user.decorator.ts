33b890257a7e6a760b0d7bdd5321517fd8eccaa0
youngprinnce <ajiboyeadedotun16@gmail.com>
2024-03-25 00:57:09 +0100

    Add GetUser decorator and JwtStrategy

---
+++ src/users/get-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    delete user.password;
+    return user;
+  },
+);
+
+
---
+++ src/users/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import {  Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { jwtPayload } from './jwt-payload.interface';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+   @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'secret',
+    });
+  }
+
+  async validate(payload: jwtPayload): Promise<User> {
+    const { email, id } = payload;
+    const user = await this.userRepository.findOne({ where: { email, id } });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
